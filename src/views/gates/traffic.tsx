c0f5e5fe58538d5a3dd295d20888dadcd76a45fb
devhtsco <dev@hamgamkhodro.com>
2020-09-05 16:40:22 +0430

    0.11.0

--- src/views/gates/traffic.tsx
+++ src/views/gates/traffic.tsx
@@ -4,12 +4,25 @@ import './styles/index.css'
 import * as Icon from 'react-bootstrap-icons';
 import { Layout, Loading, Toast } from '../layout/layout'
 import { PersianCalendar } from '../../components/cute-ui/persianCalendar/persianCalendar';
+import { Basic, Table } from '../../components/cute-ui/cuteUI';
 
 export function Traffic({ authorize = false }: { authorize: boolean }) {
 
     return (
-        <Layout isAuthenticated={authorize} title='تردد‌ها' icon={Icon.House}>
-
+        <Layout isAuthenticated={authorize} title='تردد‌ها' icon={Icon.House} style={{ padding: 5 }}>
+            <Table.Table className='text-small' border type={Basic.type.dark}>
+                <Table.THead>
+                    <Table.Tr>
+                        <Table.Th width={30}>#</Table.Th>
+                        <Table.Th width={120}>زمان تردد</Table.Th>
+                        <Table.Th width={100}>نحوه شناسایی</Table.Th>
+                        <Table.Th width={200}>مکان ورود</Table.Th>
+                        <Table.Th>نام و نام خانوادگی</Table.Th>
+                        <Table.Th width={30}></Table.Th>
+                    </Table.Tr>
+                </Table.THead>
+                <Table.TBody></Table.TBody>
+            </Table.Table>
         </Layout>
     )
 }
